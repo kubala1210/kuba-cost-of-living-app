512f14329162d5805f6acf21bdc07e35969c2d4b
Jakub Gromala <jakubgromala@MacBook-Pro-Jakub-2.local>
2025-04-01 15:29:20 +0200

    update_01

--- src/calculator.py
+++ src/calculator.py
@@ -2,17 +2,77 @@ from households import Household
 
 class CostCalculator(Household):
 
-    def __init__(self, country='', city_name='', cost_of_living=0, cost_of_rent=0, total_cost=0,
-                 minimum_salary=0, average_salary=0, family_type='', r=1):
-        super().__init__(country, city_name, cost_of_living, cost_of_rent, total_cost,
-                         minimum_salary, average_salary, family_type, r)
-
     def main_calc(self):
         self.cost_counter()
         self.minimum_salary_counter()
-        print(self.wealth_level_counter())
+        self.wealth_level_counter()
 
+class Single(CostCalculator):
 
-household_1 = CostCalculator()
-household_1.main_calc()
+    def cost_counter(self):
+        print('Obliczenia minimalnego wynagrodzenia oraz wskaźnika zamożności dla singla')
+        super().cost_counter()
 
+    def minimum_salary_counter(self):
+        super().minimum_salary_counter()
+        print(f'Minimalne wynagrodzenie dla singla to: {self.minimum_salary}')
+
+    def wealth_level_counter(self, wealth_level=0, result=''):
+        self.multiplier = 1
+        super().wealth_level_counter()
+        print(result)
+
+class Couple(CostCalculator):
+
+    def cost_counter(self):
+        print('Obliczenia minimalnego wynagrodzenia oraz wskaźnika zamożności dla pary')
+        super().cost_counter()
+
+    def minimum_salary_counter(self):
+        super().minimum_salary_counter()
+        print(f'Minimalne wynagrodzenie dla pary to: {self.minimum_salary}')
+
+    def wealth_level_counter(self, wealth_level=0, result=''):
+        self.multiplier = 2
+        super().wealth_level_counter()
+        print(result)
+
+class Family2plus1(CostCalculator):
+
+    def cost_counter(self):
+        print('Obliczenia minimalnego wynagrodzenia oraz wskaźnika zamożności dla rodziny 2+1')
+        super().cost_counter()
+
+    def minimum_salary_counter(self):
+        super().minimum_salary_counter()
+        print(f'Minimalne wynagrodzenie dla rodziny 2+1 to: {self.minimum_salary}')
+
+    def wealth_level_counter(self, wealth_level=0, result=''):
+        self.multiplier = float(input('Podaj liczbę osób zarabiających w gospodarstwie: '))
+        super().wealth_level_counter()
+        print(result)
+
+class Family2plus2(CostCalculator):
+
+    def cost_counter(self):
+        print('Obliczenia minimalnego wynagrodzenia oraz wskaźnika zamożności dla rodziny 2+2')
+        super().cost_counter()
+
+    def minimum_salary_counter(self):
+        super().minimum_salary_counter()
+        print(f'Minimalne wynagrodzenie dla rodziny 2+2 to: {self.minimum_salary}')
+
+    def wealth_level_counter(self, wealth_level=0, result=''):
+        self.multiplier = float(input('Podaj liczbę osób zarabiających w gospodarstwie: '))
+        super().wealth_level_counter()
+        print(result)
+
+single_01 = Single()
+couple_01 = Couple()
+family_01 = Family2plus1()
+family_02 = Family2plus2()
+
+single_01.main_calc()
+couple_01.main_calc()
+family_01.main_calc()
+family_02.main_calc()
