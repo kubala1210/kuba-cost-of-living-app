8fc576da00345926271b4cc08e72a8736457e2ab
Jakub Gromala <jakubgromala@MacBook-Pro-Jakub-2.local>
2025-04-01 10:12:18 +0200

    add_files_01

---
+++ src/calculator.py
@@ -0,0 +1,4 @@
+class CostCalculator:
+    def __init__(self, city, household):
+        self.city = city
+        self.household = household
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from src.models import CityData
+from src.households import Single, Family2Plus2
+from src.calculator import CostCalculator
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
